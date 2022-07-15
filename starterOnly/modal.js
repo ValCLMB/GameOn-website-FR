bae3da9b412e38e0d8c9a5d2026fc8242642f2d0
ValCLMB <valentin.coulomb2@gmail.com>
2022-07-15 12:45:14 +0200

    Change the selections for inputs

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -15,6 +15,14 @@ const closeModalBtn = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
 const form = document.querySelector(".modal-body form");
 
+// Form inputs
+const first = document.querySelector('#first')
+const last = document.querySelector('#last')
+const email = document.querySelector('#email')
+const quantity = document.querySelector('#quantity')
+const locationRadios = document.querySelectorAll("input[type='radio']")
+const conditionCheck = document.querySelector("#checkbox1");
+
 //EVENT LISTENERS
 
 // launch modal event
@@ -73,46 +81,11 @@ function radioChecked(radios) {
 // on submit form data verification
 function submitForm(e) {
     e.preventDefault();
-    const form = e.target;
-    // All form inputs
-    let first;
-    let last;
-    let email;
-    let quantity;
-    let location = [];
-    let checkbox;
-    // Useful var for validation
 
-
-    // Loop in the object and assign the variable to DOM item according to input "name" attribute
-    Object.values(form).forEach(input => {
-        input.value.trim();
-        switch (input.name) {
-            case "first" :
-                first = input;
-                break;
-          case "last" :
-            last = input;
-            break;
-          case "email" :
-            email = input;
-            break;
-          case "quantity" :
-            quantity = input;
-            break;
-            case "location" :
-                location.push(input);
-                break;
-            case "checkbox1" :
-                checkbox = input;
-                break;
-        }
-    })
-
-    // Return true if all the condition is respected, false if not
+    // Return true if all the inputs is valid , false if not
     const formValid = checkLength(first, 2) && checkLength(last, 2)
     && checkEmail(email) && checkNumber(quantity)
-    && radioChecked(location) && checkbox.checked ? true : false;
+    && radioChecked(locationRadios) && conditionCheck.checked ? true : false;
 
     // If the form is valid close the modal and clear the inputs values
     if (formValid) {
